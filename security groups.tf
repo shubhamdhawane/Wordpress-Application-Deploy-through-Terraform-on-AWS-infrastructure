6dd9ab76c703875dd6112deb78e0611036b1e4cf
shubham dhawane <121227465+shubhamdhawane@users.noreply.github.com>
2023-12-10 22:32:09 +0530

    Update security groups.tf

--- security groups.tf
+++ security groups.tf
@@ -1,10 +1,10 @@
 #####################################
 # security group for bastion-server
 #####################################
-resource "aws_security_group" "wp-bastion-sg" {
+resource "aws_security_group" "skyage-bastion-sg" {
   name        = "bastion-sg"
   description = "Allow port 22 from anywhere"
-  vpc_id      = aws_vpc.wp_vpc.id
+  vpc_id      = aws_vpc.skyage.id
   tags = {
     "Name" = "sg-for-bastion-server"
   }
@@ -28,10 +28,10 @@ resource "aws_security_group" "wp-bastion-sg" {
 ############################
 # security group for ALB
 ############################
-resource "aws_security_group" "wp-ALB-SG" {
+resource "aws_security_group" "skyage-ALB-SG" {
   name        = "ALB-SG"
   description = "Allow port 80 and 443 from anywhere"
-  vpc_id      = aws_vpc.wp_vpc.id
+  vpc_id      = aws_vpc.skyage.id
   tags = {
     "Name" = "sg-for-pr8-alb"
   }
@@ -64,10 +64,10 @@ resource "aws_security_group" "wp-ALB-SG" {
 ###################################
 # security group for app-servers
 ###################################
-resource "aws_security_group" "wp-App-SG" {
+resource "aws_security_group" "skyage-App-SG" {
   name        = "App-SG"
   description = "Allow port 22 from bastion-sg and 80 from ALB-SG"
-  vpc_id      = aws_vpc.wp_vpc.id
+  vpc_id      = aws_vpc.skyage.id
   tags = {
     "Name" = "sg-for-app-servers"
   }
@@ -75,14 +75,14 @@ resource "aws_security_group" "wp-App-SG" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    security_groups = [aws_security_group.wp-bastion-sg.id]
+    security_groups = [aws_security_group.skyage-bastion-sg.id]
     description     = "allow-trrafic-from-bastion-sg-only"
   }
   ingress {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    security_groups = [aws_security_group.wp-ALB-SG.id]
+    security_groups = [aws_security_group.skyage-ALB-SG.id]
     description     = "allow-trrafic-from-ALB-SG-only"
   }
   /*
@@ -90,7 +90,7 @@ resource "aws_security_group" "wp-App-SG" {
     from_port       = 443
     to_port         = 443
     protocol        = "tcp"
-    security_groups = [aws_security_group.wp-ALB-SG.id]
+    security_groups = [aws_security_group.skyage-ALB-SG.id]
     description     = "allow-trrafic-from-ALB-SG-only"
   }
   */
@@ -107,10 +107,10 @@ resource "aws_security_group" "wp-App-SG" {
 ###################################
 # security group for RDS-server
 ###################################
-resource "aws_security_group" "wp-DB-SG" {
+resource "aws_security_group" "skyage-DB-SG" {
   name        = "DB-SG"
   description = "Allow port 3306 from App-SG only"
-  vpc_id      = aws_vpc.wp_vpc.id
+  vpc_id      = aws_vpc.skyage.id
   tags = {
     "Name" = "sg-for-RDS-server"
   }
@@ -118,7 +118,7 @@ resource "aws_security_group" "wp-DB-SG" {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    security_groups = [aws_security_group.wp-App-SG.id]
+    security_groups = [aws_security_group.skyage-App-SG.id]
     description     = "allow-trrafic-from-App-SG-only"
   }
   egress {
