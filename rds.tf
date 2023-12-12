a80c67b372678fc5c9e3575633729f05a4445e9d
shubham dhawane <121227465+shubhamdhawane@users.noreply.github.com>
2023-12-12 11:05:16 +0530

    Update rds.tf
    change zone value

--- rds.tf
+++ rds.tf
@@ -18,13 +18,13 @@ resource "aws_db_instance" "db_instance" {
   engine                   = "mysql"
   db_name                  = "skyage_RDS_DB"
   username                 = "admin"
-  password                 = "admin"
+  password                 = "admin123"
   instance_class           = "db.t3.micro"
   allocated_storage        = 20
   db_subnet_group_name     = aws_db_subnet_group.db_sub_group.id
   multi_az                 = false
   vpc_security_group_ids   = [aws_security_group.skyage-DB-SG.id]
-  availability_zone        = "us-east-1c"
+  availability_zone        = "us-east-1b"
   port                     = 3306
   publicly_accessible      = false
   skip_final_snapshot      = true
