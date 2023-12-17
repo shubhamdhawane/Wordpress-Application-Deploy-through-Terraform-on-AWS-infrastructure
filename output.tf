c666d74babe45808fb7d9fec961b278106792bf2
shubham dhawane <121227465+shubhamdhawane@users.noreply.github.com>
2023-12-17 17:53:40 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,24 @@
+# public-ip of bastion-server
+output "bastion-server-public-ip" {
+  value = aws_instance.skyage-bastion-server.public_ip
+}
+
+# private-ip of app-server-1
+output "app-server-1-private-ip" {
+  value = aws_instance.skyage-app-server-1.private_ip
+}
+
+# private-ip of app-server-2
+output "app-server-2-private-ip" {
+  value = aws_instance.skyage-app-server-2.private_ip
+}
+
+# rds-endpoint
+output "rds_endpoint" {
+  value = aws_db_instance.db_instance.address
+}
+
+# ALB dns_name
+output "alb_dns_name" {
+  value = aws_lb.skyage_alb.dns_name
+}
