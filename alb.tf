09f6d30cc662b9c6e60c83642b5602d3712d78f5
shubham dhawane <121227465+shubhamdhawane@users.noreply.github.com>
2023-12-17 17:21:51 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -4,7 +4,7 @@ resource "aws_lb_target_group" "skyage_tg" {
   name        = "skyage-target-group"
   port        = 80
   protocol    = "HTTP"
-  vpc_id      = aws_vpc.wp_vpc.id
+  vpc_id      = aws_vpc.skyage.id
 
   stickiness { #here no need to enabled this stickiness argument,#
     #enabled = true
