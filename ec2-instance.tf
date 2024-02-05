f65d617cc6b96730d845c321f5b15b9ed402a284
shubham dhawane <121227465+shubhamdhawane@users.noreply.github.com>
2024-02-05 19:58:42 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,92 +1,57 @@
-# Data source:  Get latest AMI ID for Amazon Linux2 OS
-data "aws_ami" "amazon_linux_2" {
+ Data source: Get latest AMI ID for Ubuntu OS
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["099720109477"]  # Canonical owner ID
+
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-*-gp2"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
   filter {
     name   = "architecture"
     values = ["x86_64"]
   }
 }
 
-
-
+# Output the retrieved AMI ID
+output "latest_ubuntu_ami_id" {
+  value = data.aws_ami.ubuntu.id
+}
 # Local variables
 locals {
   user_data = <<-EOT
 #!/bin/bash -xe
 
 # System Updates
-sudo yum -y update
+sudo apt update -y
+sudo apt install apache2 -y
+EOT
 
-# STEP 1 - Install system software - including Web and DB
-sudo yum install -y mariadb-server httpd
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+# Define local variable with Cassandra installation script
+locals {
+  cassandra = <<-EOT
+#!/bin/bash -xe
 
-# STEP 2 - Web and DB Servers Online - and set to startup
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
-
-# STEP 3 - Setpassword & DB Variables
-DBName='HR_RDS_DB'
-DBUser='admin'
-DBPassword='admin123'
-DBRootPassword='Admin123root'
-DBEndpoint='${aws_db_instance.db_instance.address}'
-
-# STEP 4 - Set Mariadb Root Password
-mysqladmin -u root password $DBRootPassword
-
-# STEP 5 - Install Wordpress
-sudo wget http://wordpress.org/latest.tar.gz -P /var/www/html
-cd /var/www/html
-sudo tar -zxvf latest.tar.gz
-sudo cp -rvf wordpress/* .
-sudo rm -R wordpress
-sudo rm latest.tar.gz
-
-# STEP 6 - Configure Wordpress
-sudo cp ./wp-config-sample.php ./wp-config.php
-sudo sed -i "s/'database_name_here'/'$DBName'/g" wp-config.php
-sudo sed -i "s/'username_here'/'$DBUser'/g" wp-config.php
-sudo sed -i "s/'password_here'/'$DBPassword'/g" wp-config.php
-sudo sed -i "s/'localhost'/'$DBEndpoint'/g" wp-config.php
-
-# Step 6a - permissions 
-sudo usermod -a -G apache ec2-user    #Add your user (in this case, ec2-user) to the apache group. 
-sudo chown -R ec2-user:apache /var/www    #Change the group ownership of /var/www and its contents to the apache group.
-sudo chmod 2775 /var/www
-sudo find /var/www -type d -exec chmod 2775 {} \;
-sudo find /var/www -type f -exec chmod 0664 {} \;
-
-# STEP 7 Create Wordpress DB
-echo "CREATE DATABASE $DBName;" >> /tmp/db.setup
-echo "CREATE USER '$DBUser'@'localhost' IDENTIFIED BY '$DBPassword';" >> /tmp/db.setup
-echo "GRANT ALL ON $DBName.* TO '$DBUser'@'localhost';" >> /tmp/db.setup
-echo "FLUSH PRIVILEGES;" >> /tmp/db.setup
-mysql -u root --password=$DBRootPassword < /tmp/db.setup
-sudo rm /tmp/db.setup
+# System Updates
+sudo apt update -y
+sudo apt install openjdk-8-jre-headless -y
+sudo apt-get install curl -y
+echo "deb https://debian.cassandra.apache.org 41x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+curl https://downloads.apache.org/cassandra/KEYS | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install cassandra -y
 EOT
 }
 
-
-
 # bastion-server in public-subnet-1
 resource "aws_instance" "skyage-bastion-server" {
-  ami                         = data.aws_ami.amazon_linux_2.id
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
   key_name                    = "demo"
   subnet_id                   = aws_subnet.public_subnet1.id
@@ -100,7 +65,7 @@ resource "aws_instance" "skyage-bastion-server" {
 
 # two app-servers in private-subnets
 resource "aws_instance" "skyage-app-server-1" {
-  ami                         = data.aws_ami.amazon_linux_2.id
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
   key_name                    = "demo"
   subnet_id                   = aws_subnet.private_subnet1.id
@@ -111,14 +76,12 @@ resource "aws_instance" "skyage-app-server-1" {
   tags = {
     "Name" = "app-server-1"
   }
-  depends_on = [
-    aws_db_instance.db_instance
-  ]
+
 }
 
 
 resource "aws_instance" "skyage-app-server-2" {
-  ami                         = data.aws_ami.amazon_linux_2.id
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
   key_name                    = "demo"
   subnet_id                   = aws_subnet.private_subnet2.id
@@ -129,7 +92,36 @@ resource "aws_instance" "skyage-app-server-2" {
   tags = {
     "Name" = "app-server-2"
   }
-  depends_on = [
-    aws_db_instance.db_instance
-  ]
+
 }
+
+resource "aws_instance" "skyage-database-server-1" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.medium"
+  key_name                    = "demo"
+  subnet_id                   = aws_subnet.skyage-database-subnet-1.id
+  associate_public_ip_address = "false"
+  vpc_security_group_ids      = [aws_security_group.skyage-App-SG.id]
+  user_data                   = base64encode(local.cassandra)
+  #user_data = file("deploy-app.sh")
+  tags = {
+    "Name" = "database-server-1"
+  }
+
+}
+
+resource "aws_instance" "skyage-database-server-2" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.medium"
+  key_name                    = "demo"
+  subnet_id                   = aws_subnet.skyage-database-subnet-2.id
+  associate_public_ip_address = "false"
+  vpc_security_group_ids      = [aws_security_group.skyage-App-SG.id]
+  user_data                   = base64encode(local.cassandra)
+  #user_data = file("deploy-app.sh")
+  tags = {
+    "Name" = "database-server-2"
+  }
+
+}
+
