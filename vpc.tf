2904bd358f7d72aa5252e0ca4d945f15b6e59e3b
shubham dhawane <121227465+shubhamdhawane@users.noreply.github.com>
2024-04-09 17:11:11 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 #vpc code
 
-resource "aws_vpc" "skyage" {
+resource "aws_vpc" "skyage_vpc" {
   cidr_block = "172.16.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
@@ -10,39 +10,39 @@ resource "aws_vpc" "skyage" {
   }
 }
 
-resource "aws_subnet" "public_subnet1" {
-  vpc_id                  = aws_vpc.skyage.id
+resource "aws_subnet" "skyage-public-subnet-1" {
+  vpc_id                  = aws_vpc.skyage_vpc.id
   cidr_block              = "172.16.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "PublicSubnet1"
+    Name = "Public-Subnet-1"
   }
 }
 
-resource "aws_subnet" "public_subnet2" {
-  vpc_id                  = aws_vpc.skyage.id
+resource "aws_subnet" "skyage-public-subnet-2" {
+  vpc_id                  = aws_vpc.skyage_vpc.id
   cidr_block              = "172.16.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
   tags = {
-    Name = "PublicSubnet2"
+    Name = "Public-Subnet-2"
   }
 }
 
-resource "aws_subnet" "private_subnet1" {
-  vpc_id                  = aws_vpc.skyage.id
+resource "aws_subnet" "skyage-private-subnet-1" {
+  vpc_id                  = aws_vpc.skyage_vpc.id
   cidr_block              = "172.16.3.0/24"
   availability_zone       = "us-east-1a"
 
   tags = {
-    Name = "PrivateSubnet1"
+    Name = "Private-Subnet-1"
   }
 }
 
-resource "aws_subnet" "private_subnet2" {
-  vpc_id                  = aws_vpc.skyage.id
+resource "aws_subnet" "skyage-private-subnet-2" {
+  vpc_id                  = aws_vpc.skyage_vpc.id
   cidr_block              = "172.16.4.0/24"
   availability_zone       = "us-east-1b"
 
@@ -53,7 +53,7 @@ resource "aws_subnet" "private_subnet2" {
 
 # 2 database subnets
 resource "aws_subnet" "skyage-database-subnet-1" {
-  vpc_id            = aws_vpc.skyage.id
+  vpc_id            = aws_vpc.skyage_vpc.id
   cidr_block        = "172.16.5.0/24"
   availability_zone = "us-east-1a"
   tags = {
@@ -62,7 +62,7 @@ resource "aws_subnet" "skyage-database-subnet-1" {
 }
 
 resource "aws_subnet" "skyage-database-subnet-2" {
-  vpc_id            = aws_vpc.skyage.id
+  vpc_id            = aws_vpc.skyage_vpc.id
   cidr_block        = "172.16.6.0/24"
   availability_zone = "us-east-1b"
   tags = {
@@ -74,7 +74,7 @@ resource "aws_subnet" "skyage-database-subnet-2" {
 
 # Internet Gateway
 resource "aws_internet_gateway" "skyage_igw" {
-  vpc_id = aws_vpc.skyage.id
+  vpc_id = aws_vpc.skyage_vpc.id
   tags = {
     "Name" = "skyage-igw"
   }
@@ -87,7 +87,7 @@ resource "aws_eip" "nat_eip" {
 # Nat Gateway
 resource "aws_nat_gateway" "skyage-nat-gw" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet1.id
+  subnet_id     = aws_subnet.skyage-public-subnet-1.id
   tags = {
     Name = "skyage-nat-gw"
   }
@@ -103,7 +103,7 @@ resource "aws_nat_gateway" "skyage-nat-gw" {
 ######################################################
 # public route table
 resource "aws_route_table" "skyage-public-rt" {
-  vpc_id = aws_vpc.skyage.id
+  vpc_id = aws_vpc.skyage_vpc.id
   tags = {
     "Name" = "public-rt"
   }
@@ -112,12 +112,12 @@ resource "aws_route_table" "skyage-public-rt" {
 # Associate both public subnets with public route table
 resource "aws_route_table_association" "public_subnet_association-1" {
   route_table_id = aws_route_table.skyage-public-rt.id
-  subnet_id      = aws_subnet.public_subnet1.id
+  subnet_id      = aws_subnet.skyage-public-subnet-1.id
 }
 
 resource "aws_route_table_association" "public_subnet_association-2" {
   route_table_id = aws_route_table.skyage-public-rt.id
-  subnet_id      = aws_subnet.public_subnet2.id
+  subnet_id      = aws_subnet.skyage-public-subnet-2.id
 }
 
 # Add Internet Gateway into public route table
@@ -135,21 +135,21 @@ resource "aws_route" "skyage-route-igw" {
 ######################################################
 # private route table
 resource "aws_route_table" "skyage-private-rt" {
-  vpc_id = aws_vpc.skyage.id
+  vpc_id = aws_vpc.skyage_vpc.id
   tags = {
     "Name" = "private-rt"
   }
 }
 
 # Associate both private subnets with private route table
-resource "aws_route_table_association" "private_subnet_association-1" {
+resource "aws_route_table_association" "private_subnet_association-1" {   
   route_table_id = aws_route_table.skyage-private-rt.id
-  subnet_id      = aws_subnet.private_subnet1.id
+  subnet_id      = aws_subnet.skyage-private-subnet-1.id
 }
 
 resource "aws_route_table_association" "private_subnet_association-2" {
   route_table_id = aws_route_table.skyage-private-rt.id
-  subnet_id      = aws_subnet.private_subnet2.id
+  subnet_id      = aws_subnet.skyage-private-subnet-2.id
 }
 
 # Add Nat Gateway into private route table
@@ -166,7 +166,7 @@ resource "aws_route" "skyage-route-nat-gw" {
 ######################################################
 # database route table
 resource "aws_route_table" "skyage-database-rt" {
-  vpc_id = aws_vpc.skyage.id
+  vpc_id = aws_vpc.skyage_vpc.id
   tags = {
     "Name" = "database-rt"
   }
